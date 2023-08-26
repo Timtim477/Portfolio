d943b56c1ae7953049c675616e3bbcc784a400b1
Timtim477 <88290919+Timtim477@users.noreply.github.com>
2023-08-26 17:06:05 +0530

    Add files via upload

---
+++ src/Components/About.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Skills from './Skills.js';
+import Tilt from 'react-parallax-tilt';
+import Lottie from "lottie-react"
+import Coder from '../LottieFiles/coder.json';
+
+const About = () => {
+  return (
+    <>
+      <div className='AboutPage'>
+        <div className='AboutText'>
+          <h1 className='AboutTextHeading' >Get to <b>know</b> me!</h1>
+          <p>
+            Hi, my name is <b>Priya Kumari</b> and I am from New Jamshedpur, India.
+            I'm a <b>full stack web developer</b> and a final year college student pursuing <b>BTech in Chemical Engineering</b>. <br/><br/>
+            I have done an internship as a <b>techincal trainee</b> at TATA STEEL.
+            I love to create beautiful web development projects with attractive designs.<br/><br/>
+            I am <b>open</b> to new collaborations or work where I can contribute and grow. Feel free to connect with me, links are in the footer.<br/>
+            Apart from coding I love to do writing, you can check out some of my shots here <a href="https://www.instagram.com/devansh_sahni_/?hl=en" target='_blank'>Instagram.</a>
+          </p>
+        </div>
+
+        <div>
+          <Tilt>
+              <Lottie 
+              className="illustration" 
+              animationData={Coder} 
+              loop={true} 
+            />
+          </Tilt>
+        </div>
+
+      </div>
+      
+      <h1 className='SkillsHeading'>Professional Skillset</h1>
+      <div className='skills'>
+        
+        <Skills skill='React' />
+        <Skills skill='Node' />
+        <Skills skill='Express' />
+        <Skills skill='MongoDb' />
+        <Skills skill='Git' />
+        <Skills skill='Github' />
+        <Skills skill='Javascript' />
+        <Skills skill='C++' />
+        <Skills skill='Python' />
+        <Skills skill='Figma' />
+        <Skills skill='Vercel' />
+        <Skills skill='Npm' />
+        <Skills skill='Bootstrap'/>
+        
+      </div>
+    </>
+  )
+}
+
+export default About
